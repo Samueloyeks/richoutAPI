614313a323a7534fe2841823864f92adae9a6ba1
Sam <sammyoyekeye70@gmail.com>
2018-12-24 15:07:58 +0100

    Fixed response messages

--- controllers/utilities.js
+++ controllers/utilities.js
@@ -2,45 +2,47 @@ exports.utilitiesModel = require('../models/utilitiesModel');
 
 exports.sendResponse = function (obj) {
     let response = {};
-    
+    utilitiesModel.status = 'error';
     switch (obj.type) {
         case 'function not found':
             response.headerCode = 400;
-            response.status = 'error';
+            response.fileName = 'error';
+            response.status = utilitiesModel.status;
             response.message = ' not found on server';
             response.data = obj.data;
             break;
- 
-        case 'not found': 
+
+        case 'not found':
             response.headerCode = 404;
             // response.fileName = 'error';
-            response.status = 'error'
+            response.status = utilitiesModel.status;
             response.message = ' not found on server';
             response.data = obj.data;
             break;
-           
+
         case 'request failed':
-            response.headerCode = 404;
-            
-            //response.fileName = 'error';
-            response.status = "error";
-            response.message = utilitiesModel.message;
-            response.data = utilitiesModel.obj;
-            break;
-         /*
-        case 'request succesful':
             //header;
             // response.fileName = 'error';
             response.status = utilitiesModel.status;
-            // if obj.msg
-            response.message = obj.message;
-            //else 
+            response.message = utilitiesModel.message;
+            response.data = utilitiesModel.obj;
+            break;
+        case 'request succesful':
+            //header;
+            // response.fileName = 'error';
+            response.status = 'Successful'
+            if (obj.msg) {
+                response.message = utilitiesModel.message;
+            } else {
+                response.message = "Your request was succesful";
+            }
             response.data = utilitiesModel.obj;
             break;
         case '500':
             //header;
             // response.fileName = 'error';
-            response.status = utilitiesModel.status;
+            if (obj)
+                response.status = utilitiesModel.status;
             response.message = utilitiesModel.message;
             response.data = utilitiesModel.obj;
             break;
@@ -57,7 +59,7 @@ exports.sendResponse = function (obj) {
             response.status = utilitiesModel.status;
             response.message = utilitiesModel.message;
             response.data = utilitiesModel.obj;
-            break; */
+            break;
 
         default:
             break;
