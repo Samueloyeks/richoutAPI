15d6e4bb4e43de852a9b4f96882456d95bd0cc9c
Zino Adidi <zinoadidi@gmail.com>
2018-12-30 03:18:23 +0200

    added login function

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
 let utilities = require('../controllers/utilities');
-
 require("../models/firebase");
 let firebase = utilities.firebase;
 
@@ -15,18 +14,30 @@ exports.register = function(data){
             userModel.password = data.password;
             userModel.email = data.email;
             userModel.phoneNumber = data.phoneNumber;
+            userModel.accountType = data.accountType;
            
         }catch(ex){
             // data validation failed
         }
         firebase.auth().createUserWithEmailAndPassword(userModel.email, userModel.password)
         .then(function(result){
-            
+            firebase.database().ref(`/userProfile/` + result.user.uid).set(userModel).then(() => {
+                firebase.auth().currentUser.sendEmailVerification();
+                //Email sent
+            });
+         
             response = {
                 status:'success',
                 message:'Registration Successful',
-                data:result['user']['providerData']
+                data:{
+                    uid : result.user.uid,
+                    fullName : userModel.fullName,
+                    phoneNumber :userModel.phoneNumber,
+                    email :userModel.email,
+                    accountType :userModel.accountType
+                }   
             }
+            
             resolve(response);
             
            // var database = firebase.database();
@@ -37,23 +48,61 @@ exports.register = function(data){
             var message = "";
             var errorCode = error.code;
             var errorMessage = error.message;
-            switch (errorCode){
-                case 'auth/weak-password':
-                    message = 'The password is too weak.';
-                    break;
-                case 'auth/email-already-in-use':
-                    message = 'User already exist on the system.';
-                    break;
-                case 'auth/invalid-email':
-                    message = 'Invalid email address.';
-                    break;
-                case 'auth/operation-not-allowed':
-                    message = ' email/password accounts are not enabled.';
-                    break;
-                default:
-                    message="An error occured during registration"
-                    break;
+           
+            response = {
+                'status':'error',
+                'message':error.message,
+                'data':data
             }
+            reject(response);
+        });
+        
+        
+    });
+
+}
+
+exports.login = function(data){
+    
+    return new Promise (function(resolve,reject){
+        let userModel =  require('../models/userModel');
+        userModel = userModel.user;
+        let response = new Object();
+        try{
+            
+            userModel.password = data.password;
+            userModel.email = data.email;
+           
+        }catch(ex){
+            // data validation failed
+            console.log('login:data validation failed')
+        }
+        firebase.auth().signInWithEmailAndPassword(userModel.email, userModel.password)
+        .then(function(result){
+            firebase.database().ref(`/userProfile/` + result.user.uid).once('value').then(function(snapshot) {
+                uData = snapshot.val();
+                delete uData['password'];
+                response = {
+                    status:'success',
+                    message:'Login Successful',
+                    data:uData
+                }
+                resolve(response);
+                
+                //Email sent
+            });
+            
+            
+            
+           // var database = firebase.database();
+            //firebase.database().ref('users/' + userModel.email).set(userModel);
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var message = "";
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            
             response = {
                 'status':'error',
                 'message':error.message,
