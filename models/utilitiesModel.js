4b8213f066030135a131c52cb13aeade4c510ade
Zino Adidi <zinoadidi@gmail.com>
2018-12-30 04:38:50 +0200

    attempt to fix cors

--- models/utilitiesModel.js
+++ models/utilitiesModel.js
@@ -9,7 +9,7 @@ exports.headers = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin':'*',  
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Methods': 'POST,OPTIONS',
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Methods': 'POST'
 }
